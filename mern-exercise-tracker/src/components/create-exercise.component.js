929d7483b4a7e1280c49cd8dfc34c2d282874c3e
Pat P <sell445@gmail.com>
2019-10-23 10:39:58 -0400

    completed tutorial

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
+
 
 export default class CreateExercise extends Component {
     constructor(props) {
@@ -16,10 +18,15 @@ export default class CreateExercise extends Component {
     };
 
     componentDidMount = () => {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(res => {
+                if(res.data.length > 0) {
+                    this.setState({
+                        users: res.data.map(user => user.username),
+                        username: res.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername = (e) => {
@@ -55,6 +62,10 @@ export default class CreateExercise extends Component {
             duration: this.state.duration,
             date: this.state.date,
         }
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err));
 
         console.log(exercise);
 
